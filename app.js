df2e82b48a7686ba2a4362e932f5feaf658ad287
mpedersen15 <mpedersen@mindrocketdesign.com>
2017-01-04 16:05:25 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+console.log('starting app.js...');
+
+const fs = require('fs');
+const _ = require('lodash');
+// const yargs = require('yargs');
+
+const notes = require('./notes.js');
+
+// const argv = yargs.argv;
+console.log(process.argv);
+var command = process.argv[2];
+console.log('Command: ', command);
+console.log('Process: ', process.argv);
+// console.log('Yargs', argv);
+
+if (command === 'add'){
+	console.log('Adding note');
+}else if (command === 'list'){
+	console.log('Listing all notes');
+}else if (command === 'read'){
+	console.log('Reading note');
+}else if (command === 'remove'){
+	console.log('Removing note');
+}else{
+	console.log('Command not recognized');
+}
