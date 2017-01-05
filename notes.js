b6d4915a3ada4b718e39b23a7b94ca587c527d8f
Matt Pedersen <mpedersen@mindrocketdesign.com>
2017-01-04 22:09:44 -0700

    added functions to notes.js, calls in app.js

--- notes.js
+++ notes.js
@@ -1,6 +1,23 @@
 console.log('starting notes.js...');
 
-module.exports.addNote = () => {
-	console.log('Add note');
-	return 'New note';
+var addNote = (title, body) => {
+	console.log(`Adding note with title, ${title}", and body, "${body}"`);
 }
+
+var getAll = () => {
+	console.log('Getting all notes...');
+}
+
+var getNote = (title) => {
+	console.log(`Getting note with title, "${title}"...`);
+}
+
+var removeNote = (title) => {
+	console.log(`Removing note with title "${title}"`);
+}
+module.exports = {
+	addNote,
+	getAll,
+	getNote,
+	removeNote
+};
